cdcfbffb9735f948e889f613649c1711ea6a861a
Daun Kim <yennakim11@gmail.com>
2024-11-10 01:37:27 -0600

    implemented routes to homepage, login, pricing, rooms, and page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Rooms from "./pages/Rooms";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return (
-    <div>kotatsu</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/rooms" element={<Rooms />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>
 }
