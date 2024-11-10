135ab949068a9ed4c83ecddab5f0165469ce542a
Daun Kim <yennakim11@gmail.com>
2024-11-10 02:20:18 -0600

    added CSS to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
